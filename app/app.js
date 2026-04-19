f15879de112374824bbf97603ff17f37a4d50a0e
Rahul Rudra <msrrahul77@gmail.com>
2026-04-19 14:37:59 +0600

    changes made

---
+++ app/app.js
@@ -0,0 +1,29 @@
+import express from "express"
+import bodyParser from "body-parser"
+import cors from "cors"
+import server from '../server/server.js'
+import config from "../config/index.js"
+import serviceRoute from "../modules/Services/services.route.js"
+import PriceRouter from "../modules/PriceSection/price.route.js"
+import TeamRouter from "../modules/TeamData/TeamRoute.js"
+
+// import  server  from "../server/server.js"
+
+const app = express()
+app.use(cors())
+app.use(express.json())
+// Parse JSON bodies
+app.use(bodyParser.json());
+
+// Parse URL-encoded bodies (for forms)
+app.use(bodyParser.urlencoded({ extended: true }));
+
+
+
+app.use("/services", serviceRoute)
+app.use("/price",PriceRouter)
+app.use("/team",TeamRouter)
+app.listen(config.PORT, () => {
+    server()
+console.log(`Server is Running ${config.PORT}`)
+})
